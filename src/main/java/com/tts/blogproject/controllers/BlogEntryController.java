b6cd905d63549df649d78327b25074c05b484184
EthanAmato <ethannamato@gmail.com>
2023-11-28 11:10:02 -0500

    some new templates

--- src/main/java/com/tts/blogproject/controllers/BlogEntryController.java
+++ src/main/java/com/tts/blogproject/controllers/BlogEntryController.java
@@ -1,6 +1,7 @@
 package com.tts.blogproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // This class will be responsible for taking in requests from the user and routing them accordingly
@@ -17,10 +18,50 @@ public class BlogEntryController {
 	// Listening in for Get requests to "/" route, Spring Boot will
 	// send the user to the index.html file found in either the blogEntry folder inside of
 	// static or templates:
+	
+	// Remember that the main difference between templates and 
+	// static is that templates are for 'filling out' with data
+	// whereas static html / files will remain 'static' and cannot
+	// be modified with data
+	
+	// To pass data from a controller method to a thymleaf
+	// template, we can include a 'Model' object to our
+	// method's input arguments. The 'Model' object
+	// kind of acts like a direct line of communication
+	// between our controller and our view - allowing us
+	// to specify key-value pairs in our java code that
+	// can be rendered into our Thymeleaf HTML:
+	@GetMapping("/test")
+	public String testHome(Model model) {
+		
+		// To add a key value pair of data to our model,
+		// so we can render code to our HTML, we can call
+		// the addAttribute Method on our mode:
+		model.addAttribute("message", "Hello from our controller");
+		
+		// We are going to be able to fill out templates like
+		// index.html using Thymeleaf
+		
+		return "blogEntry/test.html";
+	}
+	
+	
 	@GetMapping("/")
-	public String testHome() {
+	public String home() {
+		
 		return "blogEntry/index.html";
 	}
+	
+	@GetMapping("/new")
+	public String newPost() {
+		
+		return "blogEntry/new.html";
+	}
+	
+	
+	
+	
+	
 	
 	
 }
