7db051e56272dfc1ed95769d34f5713018547d07
EthanAmato <ethannamato@gmail.com>
2023-11-30 10:23:38 -0500

    finished blog project

--- src/main/java/com/tts/blogproject/controllers/BlogEntryController.java
+++ src/main/java/com/tts/blogproject/controllers/BlogEntryController.java
@@ -1,9 +1,15 @@
 package com.tts.blogproject.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tts.blogproject.models.BlogEntry;
@@ -57,7 +63,16 @@ public class BlogEntryController {
 	
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		
+		// Call the findAll() method on our blogEntryRepository
+		// which will return all of our blog posts from our DB
+		// inside of a List
+		List<BlogEntry> blogEntries = blogEntryRepository.findAll();
+		
+		// We then will add these blogEntries to our model so that we 
+		// can access them from our HTML/Thymeleaf template:
+		model.addAttribute("blogEntries", blogEntries);
 		
 		return "blogEntry/index.html";
 	}
@@ -72,14 +87,91 @@ public class BlogEntryController {
 	
 	
 	@PostMapping("/blogEntries")
-	public String createNewBlogEntry(BlogEntry blogEntry) {
+	public String createNewBlogEntry(BlogEntry blogEntry, Model model) {
 		System.out.println(blogEntry);
 		BlogEntry dbBlogEntry = blogEntryRepository.save(blogEntry);
 		
+		model.addAttribute("newBlogEntry", dbBlogEntry);
+		
+		
 		System.out.println(dbBlogEntry);
-		return "blogEntry/new.html";
+		return "blogEntry/result.html";
 	}
 	
+	
+	
+	
+	
+	// Responds to DELETE requests that we send from our HTML
+	// Remember, the @PathVariable annotation allows us to acquire data
+	// from the URL the user goes to. For example, in this case, if a user
+	// sends a DELETE request to the link http://localhost:8080/blogEntries/4, the 'id' variable
+	// in our deletePostById method will take on the value of 4
+	@DeleteMapping("/blogEntries/{id}")
+	public String deletePostById(@PathVariable Long id, Model model) {
+		
+		// First, we should check to see if the entry with id equal to the URL id exists in our database:
+		Optional<BlogEntry> blogEntryById = blogEntryRepository.findById(id);
+		
+		// Check to see if there is actually a blogEntry with id=id using the .isPresent() method
+		// from the Optional class:
+		if(blogEntryById.isPresent()) {
+			// We will get the blogEntry from our Optional variable using the .get() method:
+			BlogEntry blogEntry = blogEntryById.get();
+			// Add the blogEntry we got from DB as data to render in our view under the name 'deletedBlogEntry'
+			model.addAttribute("deletedBlogEntry", blogEntry);
+			
+			// Lastly, we will delete our blogEntry from the database:
+			blogEntryRepository.deleteById(id);
+		}
+		
+		return "blogEntry/delete.html";
+	}
+	
+	
+	// Get mapping so the user will be confronted with some UI upon pushing the 'edit this entry' button
+	// so that they can fill out a form to modify an existing post:
+	@GetMapping("/blogEntries/{id}")
+	public String editPostWithId(@PathVariable Long id, Model model) {
+		// Just like in the Delete mapping, check to see if blog Entry exists in DB:
+		Optional<BlogEntry> blogEntryById = blogEntryRepository.findById(id);
+		
+		if(blogEntryById.isPresent()) {
+			BlogEntry blogEntry = blogEntryById.get();
+			
+			model.addAttribute("blogEntry", blogEntry);
+		}	
+		
+		return "blogEntry/edit.html";
+	}
+	
+	// This is listening for PATCH requests from our thymeleaf.
+	// It will take in the updated BlogEntry object from the form as a request body
+	// and will try to update the database entry accordingly:
+	@PatchMapping("/blogEntries/{id}")
+	public String updatePostById(@PathVariable Long id, BlogEntry blogEntry, Model model) {
+		
+		Optional<BlogEntry> blogEntryById = blogEntryRepository.findById(id);
+
+		if(blogEntryById.isPresent()) {
+			// This is the current state of the blog entry as it exists in the database,
+			// whereas blogEntry (that we got from the Patch request) is the 'updated'
+			// version we want to turn the db Object into
+			BlogEntry dbBlogEntry = blogEntryById.get();
+		
+			System.out.println(dbBlogEntry);
+			
+			dbBlogEntry.setAuthor(blogEntry.getAuthor());
+			dbBlogEntry.setTitle(blogEntry.getTitle());
+			dbBlogEntry.setBlogContent(blogEntry.getBlogContent());
+			model.addAttribute("newBlogEntry", dbBlogEntry);
+
+			blogEntryRepository.save(dbBlogEntry);
+		
+		}
+		
+		return "blogEntry/result.html";
+	}
 	
 	
 	
